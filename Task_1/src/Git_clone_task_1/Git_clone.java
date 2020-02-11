dc85886c176f46f262da8135c828100c764ca026
91703 <91703@LAPTOP-DHG1FKQ3>
2020-02-11 14:17:50 +0530

    Git cloning

---
+++ Task_1/src/Git_clone_task_1/Git_clone.java
@@ -0,0 +1,8 @@
+package Git_clone_task_1;
+
+public class Git_clone {
+
+		   public static void main(String[] args) {
+		      System.out.println("Welcome to the task1 of epam which is sharing the java projects into the github !!");
+		   }
+}
